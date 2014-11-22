f89078b07779a6f0dc40bcc737221d589a8652e7
kriminal666 <piltrafiya_666@hotmail.com>
2014-11-22 20:41:28 +0100

    Cambios para hacer commit: 	new file:   .settings/org.eclipse.ltk.core.refactoring.prefs 	modificado: AndroidManifest.xml 	modificado: project.properties 	modificado: res/values/strings.xml 	modificado: src/com/iesebre/dam2/pa201415/ivan/facegoogtwitsign1/MainActivity.java facebook no funciona

--- src/com/iesebre/dam2/pa201415/ivan/facegoogtwitsign1/MainActivity.java
+++ src/com/iesebre/dam2/pa201415/ivan/facegoogtwitsign1/MainActivity.java
@@ -1,5 +1,19 @@
 package com.iesebre.dam2.pa201415.ivan.facegoogtwitsign1;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.facebook.android.AsyncFacebookRunner;
+import com.facebook.android.AsyncFacebookRunner.RequestListener;
+import com.facebook.android.DialogError;
+import com.facebook.android.Facebook;
+import com.facebook.android.Facebook.DialogListener;
+import com.facebook.android.FacebookError;
+
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -25,6 +39,20 @@ import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+	//BEGIN FACEBOOK
+	// Your Facebook APP ID
+		private static String APP_ID = "714569478638464"; // Replace with your App ID
+
+		// Instance of Facebook Class
+		private Facebook facebook = new Facebook(APP_ID);
+		private AsyncFacebookRunner mAsyncRunner;
+		String FILENAME = "AndroidSSO_data";
+		private SharedPreferences mPrefs;
+		//FACEBOOK BUTTON
+		Button btnFbLogin;
+		//END FACEBOOK CAMPS
+	
+		//BEGIN TWITTER
 	// Constants
 	/**
 	 * Register your here app https://dev.twitter.com/apps/new and get your
@@ -65,14 +93,36 @@ public class MainActivity extends Activity {
 	
 	// Alert Dialog Manager
 	AlertDialogManager alert = new AlertDialogManager();
-	
+	//END TWITTER CAMPS
+
 	
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//FACEBOOK CONTROLS
+		btnFbLogin = (Button) findViewById(R.id.btnFb);
+		mAsyncRunner = new AsyncFacebookRunner(facebook);
+		//End facebook controls
+		//BEGIN FACEBOOK CODE
+		/**
+		 * Login button Click event
+		 * */
+		btnFbLogin.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Log.d("Image Button", "button Clicked");
+				loginToFacebook();
+			}
+		});
+		
+		
+		
 		//BEGIN TWITER CODE
+
 		if (android.os.Build.VERSION.SDK_INT > 9) {
 			StrictMode.ThreadPolicy policy =
 			new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -99,7 +149,7 @@ public class MainActivity extends Activity {
 			// stop executing code by return
 			return;
 		}
-
+		
 		// All UI elements
 		btnTwitter = (Button) findViewById(R.id.btnTwitter);
 	
@@ -120,12 +170,7 @@ public class MainActivity extends Activity {
 				loginToTwitter();
 			}
 		});
-
-	
-
-	
-
-		/** This if conditions is tested once is
+        /** This if conditions is tested once is
 		 * redirected from twitter page. Parse the uri to get oAuth
 		 * Verifier
 		 * */
@@ -156,7 +201,7 @@ public class MainActivity extends Activity {
 					Log.e("Twitter OAuth Token", "> " + accessToken.getToken());
 					 //AQUI ACCIONES PARA HACER SI HAY UN LOGIN
                     Intent login = new Intent(MainActivity.this,LoginSuccess.class);
-                    this.startActivity(login);
+                     startActivity(login);
 						
 				} catch (Exception e) {
 					// Check log for login errors
@@ -166,6 +211,77 @@ public class MainActivity extends Activity {
 		}
 
 	}
+	//LOGIN FACEBOOK
+	/**
+	 * Function to login into facebook
+	 * */
+	@SuppressWarnings("deprecation")
+	public void loginToFacebook() {
+
+		mPrefs = getPreferences(MODE_PRIVATE);
+		String access_token = mPrefs.getString("access_token", null);
+		long expires = mPrefs.getLong("access_expires", 0);
+
+		if (access_token != null) {
+			facebook.setAccessToken(access_token);
+		
+
+			Log.d("FB Sessions", "" + facebook.isSessionValid());
+		}
+
+		if (expires != 0) {
+			facebook.setAccessExpires(expires);
+		}
+
+		if (!facebook.isSessionValid()) {
+			facebook.authorize(this,
+					new String[] { "email", "publish_stream" },
+					new DialogListener() {
+
+						@Override
+						public void onCancel() {
+							// Function to handle cancel event
+						}
+
+						@Override
+						public void onComplete(Bundle values) {
+							// Function to handle complete event
+							// Edit Preferences and update facebook acess_token
+							SharedPreferences.Editor editor = mPrefs.edit();
+							editor.putString("access_token",
+									facebook.getAccessToken());
+							editor.putLong("access_expires",
+									facebook.getAccessExpires());
+							editor.commit();
+
+							// Making Login button invisible
+							btnFbLogin.setVisibility(View.INVISIBLE);
+
+						;
+						}
+
+						@Override
+						public void onError(DialogError error) {
+							// Function to handle error
+
+						}
+
+						@Override
+						public void onFacebookError(FacebookError fberror) {
+							// Function to handle Facebook errors
+
+						}
+
+					});
+		}
+	}
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		facebook.authorizeCallback(requestCode, resultCode, data);
+	}
+	//END LOGIN FACEBOOK
 
 	/**
 	 * Function to login twitter
@@ -194,11 +310,11 @@ public class MainActivity extends Activity {
 			Toast.makeText(getApplicationContext(),
 					"Already Logged into twitter", Toast.LENGTH_LONG).show();
 			// Hide login button
-			//ACCIONES SI YA ESTÁ LOGGEADO EN TWITTER
-            //Creamos un intent para mandarlo a la actividad correspondiente
-            Intent stillLogged = new Intent(MainActivity.this,LoginSuccess.class);
-            this.startActivity(stillLogged);
+			  //Creamos un intent para mandarlo a la actividad correspondiente
 
+			Intent stillLogged = new Intent(MainActivity.this,LoginSuccess.class);
+	        startActivity(stillLogged);
+          
 
 	
 		}
@@ -231,5 +347,138 @@ public class MainActivity extends Activity {
 	protected void onResume() {
 		super.onResume();
 	}
+    //LOG OUT FROM FACEBOOK
+	/**
+	 * Function to Logout user from Facebook
+	 * */
+	@SuppressWarnings("deprecation")
+	public void logoutFromFacebook() {
+		mAsyncRunner.logout(this, new RequestListener() {
+			@Override
+			public void onComplete(String response, Object state) {
+				Log.d("Logout from Facebook", response);
+				if (Boolean.parseBoolean(response) == true) {
+					runOnUiThread(new Runnable() {
 
+						@Override
+						public void run() {
+							// make Login button visible
+							//wHEN LOGOUT WHAT TO DO
+							
+
+						
+						}
+
+					});
+
+				}
+			}
+
+			public void onIOException(IOException e, Object state) {
+			}
+
+			public void onFileNotFoundException(FileNotFoundException e,
+					Object state) {
+			}
+
+			public void onMalformedURLException(MalformedURLException e,
+					Object state) {
+			}
+
+			
+			public void onFacebookError(FacebookError e, Object state) {
+			}
+		});
+	}
+	//END LOGOUT FACEBOOK
+
+	/**
+	 * Get Profile information by making request to Facebook Graph API
+	 * */
+	@SuppressWarnings("deprecation")
+	public void getProfileInformation() {
+		mAsyncRunner.request("me", new RequestListener() {
+			@Override
+			public void onComplete(String response, Object state) {
+				Log.d("Profile", response);
+				String json = response;
+				try {
+					// Facebook Profile JSON data
+					JSONObject profile = new JSONObject(json);
+					
+					// getting name of the user
+					final String name = profile.getString("name");
+					
+					// getting email of the user
+					final String email = profile.getString("email");
+					
+					runOnUiThread(new Runnable() {
+
+						@Override
+						public void run() {
+							Toast.makeText(getApplicationContext(), "Name: " + name + "\nEmail: " + email, Toast.LENGTH_LONG).show();
+						}
+
+					});
+
+					
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+
+			@Override
+			public void onIOException(IOException e, Object state) {
+			}
+
+			@Override
+			public void onFileNotFoundException(FileNotFoundException e,
+					Object state) {
+			}
+
+			@Override
+			public void onMalformedURLException(MalformedURLException e,
+					Object state) {
+			}
+
+			@Override
+			public void onFacebookError(FacebookError e, Object state) {
+			}
+		});
+	}
+	/**
+	 * Function to post to facebook wall
+	 * */
+	@SuppressWarnings("deprecation")
+	public void postToWall() {
+		// post on user's wall.
+		facebook.dialog(this, "feed", new DialogListener() {
+
+			@Override
+			public void onFacebookError(FacebookError e) {
+			}
+
+			@Override
+			public void onError(DialogError e) {
+			}
+
+			@Override
+			public void onComplete(Bundle values) {
+			}
+
+			@Override
+			public void onCancel() {
+			}
+		});
+
+	}
+	/**
+	 * Function to show Access Tokens
+	 * */
+	public void showAccessTokens() {
+		String access_token = facebook.getAccessToken();
+
+		Toast.makeText(getApplicationContext(),
+				"Access Token: " + access_token, Toast.LENGTH_LONG).show();
+	}
 }
